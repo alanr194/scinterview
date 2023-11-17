df5fd390718873fcf5f471f24c64a8f260d82944
Alan Reid <github@alanmichaelreid.com>
2023-11-17 21:03:58 +0000

    switch to artifcat registry

--- cluster/gcr.tf
+++ cluster/gcr.tf
@@ -1,3 +1,5 @@
-resource "google_container_registry" "registry" {
-  location = "US"
+resource "google_artifact_registry_repository" "registry" {
+  location      = "us-east1"
+  repository_id = "scinterview"
+  format        = "DOCKER"
 }

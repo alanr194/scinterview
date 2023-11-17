d25ba48a3d4ef8f5ffaaeeba37dee9239cc72958
Alan Reid <github@alanmichaelreid.com>
2023-11-17 21:43:13 +0000

    change state path for kubernetes

--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -1,3 +1,10 @@
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "scinterview-terraform-state"
+    prefix = "terraform/kubernetes/state"
+  }
+}
